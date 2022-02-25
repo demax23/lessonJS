3aebc24cc17d0075aea2b76e50905a4017fbf20c
Maximov Denis <demax23@list.ru>
2022-02-25 12:14:25 +0300

    домашняя работа

--- lesson1/ClassWork/index.js
+++ lesson1/ClassWork/index.js
@@ -1,5 +1,7 @@
-console.log("Hello world"); 
+console.log('Hello world')
 
-const myName = "Denis";
+const myName = 'Denis'
 
-console.log(myName);
+console.log(myName)
+
+console.log('я запустил node js')
---
+++ lesson1/HomeWork/index.js
@@ -0,0 +1,9 @@
+const ourPlanetname = "Earth";
+console.log(ourPlanetname);
+
+let usersCurrenttime = 1200;
+console.log(usersCurrenttime);
+
+let numberOfarticles = 12;
+console.log(numberOfarticles);
+
