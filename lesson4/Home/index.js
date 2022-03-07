e1264dd3107d075d89c7eb17e338b58414b5945b
Maximov Denis <demax23@list.ru>
2022-03-07 18:30:53 +0300

    домашняя работа

---
+++ lesson4/Home/index.js
@@ -0,0 +1,87 @@
+// let value = -3 ;
+
+// if (value == 0) {
+//     console.log(`Верно`);
+// } else {
+//     console.log(`Неверно`);
+// }
+
+// let value = 0 ;
+
+// if (value != 0) {
+//     console.log(`Верно`);
+// } else {
+//     console.log(`Неверно`);
+// }
+
+// let promt = `test`;
+
+// if (promt != false) {
+//     console.log(`Верно`);
+// } else  {
+//     console.log(`Неверно`)
+// }
+
+
+// let value = -1
+
+// if (value > 0) {
+//     console.log(1);
+// } else if(value < 0);{
+// console.log(-1);
+// } 
+
+// const value = 1
+
+// if (value === `1`) {
+//     console.log(`Верно`)
+// } else {
+//     console.log(`Неверно`)
+// }
+// let test = true;
+
+// if (test == false) {
+//     console.log(`Верно`)
+// } else {
+//     console.log(`Неверно`)
+// }
+// let test = voule
+
+// (test == true) ? console.log(`Верно`) : console.log(`Неверно`);
+
+// let result =  == true ? console.log(`Верно`) : console.log(`Неверно`); 
+
+// let a = 6
+
+// if(a > 0 && a < 5 ) {
+//     console.log(`Верно`);
+// } else  {
+//     console.log(`Неверно`)
+// }
+
+// let a = 2
+
+// if(a == 0 || a == 2) {
+//     console.log(a + 7)
+// } else {
+//     console.log(a / 10)
+// } 
+
+// let a = 3
+// let b = 5
+
+// if( a <= 1 && b >= 3 ) {
+//     console.log( a + b)
+// } else {
+//     console.log(a - b)
+// }
+
+// let a = 15
+// let b = 10
+
+// if(a > 2 && a < 11 || b >=6 && b < 14 ) {
+//     console.log(`Верно`)
+// } else {
+//     console.log(`Неверно`)
+// }
+
