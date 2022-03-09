96d52192b60455df70e04129e127e46e68692866
Maximov Denis <demax23@list.ru>
2022-03-09 20:22:06 +0300

    домашняя работа

--- lesson4/Home/index.js
+++ lesson4/Home/index.js
@@ -85,3 +85,153 @@
 //     console.log(`Неверно`)
 // }
 
+
+// switch(num){
+//     case 1
+// }
+// const currentTmp = +25
+
+// if(currentTmp < -30){
+//     console.log(`Оставайтесь дома!`)
+// } else if (currentTmp > -30 && currentTmp <= -10){
+//     console.log(`Сегодня холодно`)
+// } else if (currentTmp > -10 && currentTmp <= +5){
+//     console.log(`Не холодно`)
+// } else if(currentTmp > +5 && currentTmp <= +15 ){
+//     console.log(`Тепло`)
+// }    else if(currentTmp > +15 && currentTmp <= +25){
+//         console.log(`Очень тепло`)   
+// } else if (currentTmp > +25 && currentTmp <= +35){
+//     console.log(`Жарко`)
+// } else if (currentTmp >= +35){
+//     console.log(`Пекло`)
+// } 
+  
+// const num = 4
+
+// switch(num){
+//     case 1 : 
+//     console.log(`Зима`)
+//     break;
+//     case 2 : 
+//     console.log(`Весна`)
+//     break;
+//     case 3 :
+//     console.log(`Лето`)
+//     break;
+//     case 4 :
+//     console.log(`Осень`)    
+// }
+
+// const day = 15
+
+// if (day >= 1 && day <=10){
+
+//     console.log(`Первая декада`)
+
+// } else if (day > 10 && day <= 20) {
+
+//     console.log(`Вторая декада`)
+// } else if (day > 10 && day <= 31 ) {
+
+//     console.log(`Третья декада`)
+// }
+
+// const month = 5
+
+// if (month >= 1 && month <= 3){
+//     console.log(`Зима`)
+// } else if( month > 3 && month <= 6){
+//     console.log(`Весна`)
+// } else if ( month > 6 && month <= 9){
+//     console.log(`Лето`)
+// } else if ( month > 9 && month <= 12){
+//     console.log(`Осень`)
+// }
+
+// const strings = `12345`
+
+// if(strings [0] === 1 || strings [0]) == 2 {
+//     console.log(`Да`)
+// } else {
+//     console.log(`Нет`)
+// } 
+
+// const num = 345
+
+// let numSumma = num[0] + num[1] + num[3]
+// console.log(numSumma) 
+
+// const str = '123';
+// const sum = (str[0]) + (str[1]) + (str[2]);
+// console.log(sum);
+
+// let num = 0  
+
+// while (num < 5 ){
+//     console.log(num);
+//     num++;
+// }
+
+// let num = 8;
+// while (num){
+//     console.log(num);
+//     num--;
+// // }
+// let num = 0
+
+// while(num < 3) {
+//     console.log(num)
+//     num++;
+// }
+
+// firstFor: for (let num = 0; num < 2; num++) {
+//     for (let size = 0; size < 3; size++) {
+//         if (num == 1){
+//             break firstFor
+//         }
+//         console.log(size);
+//     }
+
+    
+// }
+
+// let num = 0
+// while(num >= 0 && num <= 100 ){
+//     console.log(num);
+//     num ++;
+// }
+
+// for(let num = 0; num >= 0 && num <= 100; num ++ ){
+//     console.log(num)
+// }
+
+// for(let num = 11 ; num >= 11 && num <= 33; num ++){
+//     console.log(num)
+// }
+
+// let num = 11
+
+// while(num >= 11 && num <= 33 ){
+//     console.log(num)
+//     num ++;
+// }
+
+// for( let num = 0 ; num >= 0 && num <= 100; num += 2 ) {
+//     console.log(num)
+// }
+
+// let num = 0 
+
+// while( num >= 0 && num <= 100 ) {
+//     console.log(num)
+//     num += 2
+// }
+
+//  let sum = 0 
+
+//  for ( let num = 0 ; num <= 100 ; num ++){
+//      console.log(sum)
+//      sum += num;
+
+//  }
